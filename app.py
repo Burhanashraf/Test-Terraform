9d7e49cdfcee94037b92337e93190ded2d8d9e9b
Burhan Ashraf <97471833+Burhanashraf@users.noreply.github.com>
2026-05-05 12:56:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return "Hello from Flask"
+    return render_template('index.html')
+
 
 @app.route('/health')
 def health():
     return 'Server is up and running'
-
-if __name__ == "__main__":
-    print("Starting Flask server...")
-    app.run(host="0.0.0.0", port=5000)
